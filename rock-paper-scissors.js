10661c3e1d10e674af1a104d104f2faf234ec12b
Igor-D-art <gorgsemenon1@gmail.com>
2021-09-22 15:21:20 +0300

    Final version before adding UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,51 +1,73 @@
-// player inputs the selection through prompt. The selection is recorded into playerSelection variable an is uppercased
-// for the further cmparison with the computerSelection
 
-
-let playerInput =(prompt('Please choose one of the three: Rock, Paper or Scissors'));
-const playerSelection = playerInput.toUpperCase();
-console.log (playerSelection);
-
-// Constant that makes computer selection by assigning random number within 0, 1 or 2 and turning it into ROCK, PAPER
-// or SCISSORS
-
-let computerSelection = computerPlay();
-
-function computerPlay() {
-   let compInput= Math.floor(Math.random()*3);
-   if (compInput ==2) {
-    compInput='SCISSORS'
- } else if (compInput ==1) {
-    compInput='PAPER'
- } else {compInput="ROCK"}
-   return(compInput);
- }
-console.log(computerSelection);
+  let roundCounter =1;
+  let computerScore=0;
+  let userScore=0;
 
 // Function playRound() gets user's and computer's selections and defines the winner throwing an alert
 
-function playRound(playerSelection, computerSelection) {
-   let roundResult; 
-    if (playerSelection == "ROCK" && computerSelection=="PAPER") {
-           roundResult='You loose, paper beats rock!'
-       } else if (playerSelection == "PAPER" && computerSelection=="SCISSORS") {
-        roundResult='You loose, scissors beat paper!'
-       } else if (playerSelection == "SCISSORS" && computerSelection=="ROCK") {
-        roundResult='You loose, rock beats scissors!'
-       } else if (playerSelection == "ROCK" && computerSelection=="ROCK") { 
-        roundResult='Rock vs rock, it is a draw!'
-       } else if (playerSelection == "PAPER" && computerSelection=="PAPER") { 
-        roundResult='Paper vs paper, it is a draw!'
-       } else if (playerSelection == "SCISSORS" && computerSelection=="SCISSORS") { 
-        roundResult='Scissors vs scissors, it is a draw!' 
-       } else if (playerSelection !=='ROCK' && playerSelection !=='PAPER' && playerSelection!=='SCISSORS') {
-           roundResult='Please enter a valid selection i ne pytaysya naebat computer'
-       } else { roundResult='You win, congrats!' }
+playGame();
 
-       return alert(roundResult);
+function playGame () {
+    while (computerScore<5 && userScore<5) {
+        console.log('\n ROUND ' + roundCounter);
+        console.log("Computer score: " + computerScore);
+        console.log("User's score: " + userScore);
+        playRound();
+    } 
+    if (computerScore==5) {
+      alert('You loose the game. Coputer won 5 rounds. Want to play one more time - please reload the page =)');
+     } else {
+      alert('You won 5 rounds! Congrats! Want to play one more time - please reload the page =)');
+     }
     }
-    
-  console.log(playRound(playerSelection, computerSelection));
 
-  // function game() 
-   
+function playRound() {
+    userPlay();
+    console.log('Player selection= ' + playerSelection);
+    computerPlay();
+    console.log('Computer selection= ' +computerSelection);
+   let roundResult=''; 
+    if (playerSelection == "ROCK" && computerSelection=="PAPER") {
+           roundResult='You loose, paper beats rock!';
+           computerScore++;
+       } else if (playerSelection == "PAPER" && computerSelection=="SCISSORS") {
+        computerScore++;
+        roundResult=('You loose, scissors beat paper!' + '\nYour score: ' + userScore + '\nComputer score: ' + computerScore);
+       } else if (playerSelection == "SCISSORS" && computerSelection=="ROCK") {
+        computerScore++;
+        roundResult=('You loose, rock beats scissors!' + '\nYour score: ' + userScore + '\nComputer score: ' + computerScore);
+       } else if (playerSelection == "ROCK" && computerSelection=="ROCK") { 
+        roundResult=('Rock vs rock, it is a draw!' + '\nYour score: ' + userScore + '\nComputer score: ' + computerScore)
+       } else if (playerSelection == "PAPER" && computerSelection=="PAPER") { 
+        roundResult=('Paper vs paper, it is a draw!' + '\nYour score: ' + userScore + '\nComputer score: ' + computerScore)
+       } else if (playerSelection == "SCISSORS" && computerSelection=="SCISSORS") { 
+        roundResult=('Scissors vs scissors, it is a draw!' + '\nYour score: ' + userScore + '\nComputer score: ' + computerScore) 
+       } else if (playerSelection !=='ROCK' && playerSelection !=='PAPER' && playerSelection!=='SCISSORS') {
+           roundResult=('Please enter a valid selection i ne pytaysya naebat computer' + '\nYour score: ' + userScore + '\nComputer score: ' + computerScore)
+       } else { 
+        userScore++;  
+        roundResult=('You win, congrats!' + '\nYour score: ' + userScore + '\nComputer score: ' + computerScore);
+       }
+       roundCounter++;
+       return alert(roundResult);
+
+    }
+
+    function userPlay(){
+        let playerInput =(prompt('Please choose one of the three: Rock, Paper or Scissors'));
+        playerSelection=playerInput.toUpperCase();
+        return(playerSelection);
+        }
+
+      
+        function computerPlay() {
+           let compInput= Math.floor(Math.random()*3);
+           if (compInput ==2) {
+            computerSelection='SCISSORS'
+         } else if (compInput ==1) {
+            computerSelection='PAPER'
+         } else {computerSelection="ROCK"}
+           return(computerSelection);
+         }
+         
+
